db261c963ad08203bf49571c79418987352d1a04
ADEP-123 <andep210399@gmail.com>
2024-03-06 09:07:26 -0500

    feat: :sparkles: panel de carga de la aplicacion con animacion

--- Scripts/index.js
+++ Scripts/index.js
@@ -7,50 +7,55 @@ import renderOldList from "./renderOldList.js";
 import currentTime from "./setTime.js";
 import iniciadorApp from "./iniciadorApp.js";
 
-// Listener para iniciar la app
-document.querySelector("#submitNameBtn").addEventListener("click", () => {
-    const userName = document.querySelector("#userNameInput").value;
-    if (userName.trim() !== "") {
-        iniciadorApp(userName)
-    }
-});
+document.addEventListener("DOMContentLoaded", () => {
+    // Listener para iniciar la app
+    document.querySelector("#submitNameBtn").addEventListener("click", () => {
+        const userName = document.querySelector("#userNameInput").value;
+        if (userName.trim() !== "") {
+            iniciadorApp(userName)
+        } else {
+            alert("Por favor ingresa un nombre para continuar")
+        }
+    });
 
 
-// Llamando a la funcion para setear la fecha
-setDate()
+    // Llamando a la funcion para setear la fecha
+    setDate()
 
-// Llamando funcion que crea el reloj
-setInterval(currentTime, 1000);
+    // Llamando funcion que crea el reloj
+    setInterval(currentTime, 1000);
 
-// Render from local Storage
-let data = localStorage.getItem("ToDo")
-renderOldList(data)
+    // Render from local Storage
+    let data = localStorage.getItem("ToDo")
+    renderOldList(data)
 
-// Listeners para agregar tareas
-variables.butAddNewTask.addEventListener("click", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    addTask(variables.inNewTask.value,false);
-})
+    // Listeners para agregar tareas
+    variables.butAddNewTask.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        addTask(variables.inNewTask.value, false);
+    })
 
-document.addEventListener("keyup", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    if (e.key == "Enter") {
-        addTask(variables.inNewTask.value,false);
-    }
-})
+    document.addEventListener("keyup", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (e.key == "Enter") {
+            addTask(variables.inNewTask.value, false);
+        }
+    })
 
-// Listener para agregar o eliminar tareas
-variables.taskList.addEventListener("click", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    const element = e.target;
-    const elementData = element.attributes.data.value;
-    if (elementData === "realizado") {
-        completeTask(element);
-    } else if (elementData === "eliminado") {
-        deleteTask(element)
-    }
+    // Listener para agregar o eliminar tareas
+    variables.taskList.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const element = e.target;
+        const elementData = element.attributes.data.value;
+        if (elementData === "realizado") {
+            completeTask(element);
+        } else if (elementData === "eliminado") {
+            deleteTask(element)
+        }
+
+    })
 
 })
--- Scripts/iniciadorApp.js
+++ Scripts/iniciadorApp.js
@@ -3,7 +3,11 @@ const iniciadorApp = (nombre) => {
     variables.setUserName(nombre);
     document.querySelector("#mensBievenida").innerHTML = `Bienvenido ${variables.userName}`
     document.querySelector("#initialView").style.display = "none";
-    document.querySelector("#container").style.display = "flex";
+    document.querySelector("#transitionContainer").style.display = "flex";
+    setTimeout(() => {
+        document.querySelector("#transitionContainer").style.display = "none";
+        document.querySelector(".container").style.cssText = "display: flex; flex-direction: column;";      
+    }, 4000);
 
 }
 export default iniciadorApp;
